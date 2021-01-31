3cad088154f74b57af7dec0fd2a5f7f2e21dc397
Arisa Fukuzaki <25793187+schabibi1@users.noreply.github.com>
2021-01-31 23:42:35 +0100

    close #2

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -6,20 +6,22 @@ const ContactForm = () => {
   return (
     <div>
       <form 
-        name="contact"
-        method="post"
-        data-netlify="true"
-        netlify-honeypot="bot-field"
-        action="/success"
+        // name="contact"
+        // method="post"
+        // data-netlify="true"
+        // netlify-honeypot="bot-field"
+        // action="/success"
+        action="https://getform.io/f/6746bcaf-e462-4c55-ad6a-f133ec83f13c"
+        method="POST"
       >
-        <input type="hidden" name="form-name" value="contact" />
+        {/* <input type="hidden" name="form-name" value="contact" />
 				<p className="hidden" className={contactFormStyles.formFlex}>
 					<label>Don’t fill this out if you're human. <br />この項目は空欄でお願いします: </label>
           <input name="bot-field" />
-				</p>
+				</p> */}
         <p className={contactFormStyles.formFlex}>
 					<label>Name: </label>
-          <input type="text" name="name" placeholder="Your name" />
+          <input type="text" name="name" placeholder="i.e. Robin Brawn" />
 				</p>
 				<p className={contactFormStyles.formFlex}>
 					<label>Email: </label>
@@ -27,7 +29,7 @@ const ContactForm = () => {
 				</p>
 				<p className={contactFormStyles.formFlex}>
 					<label>Message: </label>
-          <textarea name="message"></textarea>
+          <input type="text" name="message" placeholder="Write your message" />
 				</p>
 				<p>
 					<button className={contactFormStyles.contactButton} type="submit">Send</button>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -13,8 +13,8 @@ const ContactPage = () => {
 			<h1 className={contactStyles.topTitle}>Contact Me</h1>
 			<div>
 				I work as a freelance. If you need a developer for your webs and web apps, just contact me through Twitter / Github / a contact form in below. I'd love to help you.
-				<br />フリーランスのエンジニアとしてドイツで仕事をしています。web開発やwebアプリ開発、プログラミング学習の個別サポート、オンラインプログラミングコースのご受講をご希望でしたら、お気軽にTwitter、Github、もしくは以下のフォームからご連絡ください。
-				<br />フリーランス、海外エンジニア転職などのご相談は、MENTAにて 1,000円/単発で承っております👉 <a href="https://menta.work/plan/2157" target="_blank" rel="noopener noreferrer">MENTAご相談受付ページ</a>
+				<br />フリーランスのエンジニアとしてドイツで仕事をしています。web開発やwebアプリ開発、プログラミング学習の個別サポート、Lilacコースのご受講をご希望でしたら、お気軽にTwitter、Github、もしくは以下のフォームからご連絡ください。
+				<br />フリーランス、海外エンジニア転職などのご相談は、MENTAにて 1,000円/単発(チャット)で承っております👉 <a href="https://menta.work/plan/2157" target="_blank" rel="noopener noreferrer">MENTAご相談受付ページ</a>
 			</div>
 
 			<button className={contactStyles.twitterColor}>
